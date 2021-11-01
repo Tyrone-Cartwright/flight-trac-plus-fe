f596d3ff9d1354f6ce8eb639e5451aec7fa83164
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-11-01 09:54:30 -0400

    wip: add import statements and a Footer component

--- flight-trac-plus/src/App.js
+++ flight-trac-plus/src/App.js
@@ -1,12 +1,21 @@
+import { useState, useEffect } from 'react';
+import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+import Dasboard from './pages/Dashboard';
+import Login from './pages/Login';
+import { auth } from './services/firebase';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
