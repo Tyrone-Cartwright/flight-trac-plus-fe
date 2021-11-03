a15e73af4dcf1e3f31d93b5d7cc4a78c82517c3b
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-11-03 13:50:24 -0400

    wip: added some destination images and added more style to project

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,23 @@
-const Footer = (props) => {
+const Footer = () => {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className='footer'>
+      <ul>
+        <li>
+          <button>Home</button>
+        </li>
+        <li>
+          <button>Suggestion Box</button>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <button>Privacy Policy</button>
+        </li>
+        <li>
+          <button>Giveaway</button>
+        </li>
+      </ul>
     </footer>
   );
 };
--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import greece from '../assets/greece.jpg';
+import stay from '../assets/stay.jpg';
+import vacay from '../assets/vacay.jpg';
 
 const Destinations = () => {
-  return <div>destinations</div>;
+  return (
+    <>
+      <section className='destinations'>
+        <h3>Catering to your desired locations</h3>
+        <div className='grid-dest'>
+          <div>
+            <img src={greece} alt='greece' />
+            <h4>Fly to Greece</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
+              quasi impedit laboriosam eaque ullam et.
+            </p>
+          </div>
+          <div>
+            <img src={stay} alt='greece' />
+            <h4>Fly to Aruba</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
+              quasi impedit laboriosam eaque ullam et.
+            </p>
+          </div>
+          <div>
+            <img src={vacay} alt='greece' />
+            <h4>Fly to Mombasa</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
+              quasi impedit laboriosam eaque ullam et.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Destinations;
